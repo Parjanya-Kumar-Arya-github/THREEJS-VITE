29bcc751733195fc8aaeac4781a9f27c3a8ef0d4
Parjanya Kumar Arya <Parjanya789@gmail.com>
2026-07-27 21:57:26 +0530

    Box and DodecaHedron

--- Basics/main.js
+++ Basics/main.js
@@ -1,5 +1,33 @@
 import * as THREE from 'three';
 
-const canvas: HTMLElement = document.getElementById('canvas');
+const canvas = document.getElementById('canvas');
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color("#F0F0F0");
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+const geometry = new THREE.DodecahedronGeometry();
+const material = new THREE.MeshLambertMaterial({ color: 0x468585, emissive: 0x468585});
+const dodecahedron = new THREE.Mesh(geometry, material);
+
+
+const boxGeometry = new THREE.BoxGeometry(2,0.1,2);
+const boxMaterial = new THREE.MeshLambertMaterial({ color: "#468585" });
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+box.position.y = -1.5;
+
+
+scene.add(dodecahedron);
+scene.add(box);
+
+const light = new THREE.SpotLight("#006769", 100);
+light.position.set(1, 1, 1);
+scene.add(light);
+
+
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+renderer.render(scene, camera);
