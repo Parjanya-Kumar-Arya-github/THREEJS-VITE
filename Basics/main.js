76919a3d2eb6938a72eac3f76a479ab1d8df335f
Parjanya Kumar Arya <Parjanya789@gmail.com>
2026-07-27 22:03:32 +0530

    Animation + OrbitControl

--- Basics/main.js
+++ Basics/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const canvas = document.getElementById('canvas');
 
@@ -30,4 +31,26 @@ scene.add(light);
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
 
+renderer.setPixelRatio(window.devicePixelRatio);
 renderer.render(scene, camera);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.enableZoom = true;
+controls.enablePan = true;
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    dodecahedron.rotation.x += 0.01;
+    dodecahedron.rotation.y += 0.01;
+
+    box.rotation.y += 0.005;
+
+    controls.update();
+
+    renderer.render(scene, camera);
+}
+
+animate();
