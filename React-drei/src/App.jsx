47af3b2f2e82ee9c59bd3af2e214bbf9a8ec11cf
Parjanya Kumar Arya <Parjanya789@gmail.com>
2026-07-27 22:38:43 +0530

    Final

--- React-drei/src/App.jsx
+++ React-drei/src/App.jsx
@@ -1,122 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import {Canvas, useFrame} from "@react-three/fiber";
+import {OrbitControls} from "@react-three/drei";
+import {useRef} from "react";
+import {Sparkles} from "@react-three/drei";
 
-function App() {
-  const [count, setCount] = useState(0)
+const RotatingCube = () => {
+  const meshRef = useRef();
+
+  useFrame(()=> {
+    if (meshRef.current) {
+      meshRef.current.rotation.x += 0.01;
+      meshRef.current.rotation.y += 0.01;
+    }
+  })
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+    <mesh ref={meshRef}>
+      <cylinderGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={"#468585"} emissive={"#468585"} />
+      <Sparkles count={100} scale={1} size={6} speed={0.002} noise={0.2} color={"#9CDBA6"} />
+    </mesh>
+  );
 }
 
-export default App
+
+const App = () => {
+  return (
+    <Canvas style={{height: '100vh', width: '100vw',display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+      <OrbitControls enableZoom={true} enablePan={true} enableRotate={true} />
+      <directionalLight position={[1, 1, 1]} intensity={1} color={"#9CDBA6"} />
+      <color attach="background" args={["#F0F0F0"]} />
+
+      <RotatingCube/>
+    </Canvas>
+  );
+}
+
+export default App;
